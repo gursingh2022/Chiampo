27dea83a6102b6a35056ed40b84719d87cd43d01
Gurpreet Singh <gur.singh2022@gmail.com>
2020-11-07 23:56:10 +0000

    Setup TypeScript, UIKit & Counter App

--- TestProject/wwwroot/js/counter/counter.js
+++ TestProject/wwwroot/js/counter/counter.js
@@ -1,1 +1,14 @@
-﻿
+var increment = document.getElementById('btn-increment');
+var decrement = document.getElementById('btn-decrement');
+var reset = document.getElementById('btn-reset');
+var counter = document.getElementById('counter');
+increment.addEventListener('click', function () {
+    counter.textContent = (parseInt(counter.textContent) + 1).toString();
+});
+decrement.addEventListener('click', function () {
+    counter.textContent = (parseInt(counter.textContent) - 1).toString();
+});
+reset.addEventListener('click', function () {
+    counter.textContent = '0';
+});
+//# sourceMappingURL=counter.js.map
